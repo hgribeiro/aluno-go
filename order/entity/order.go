552b070205284f3d0545f8aab92e99e0a2816d65
hgribeiro <hgribeiro@live.com>
2024-05-11 17:29:50 -0300

    func inicais e tests

--- order/entity/order.go
+++ order/entity/order.go
@@ -1,8 +1,42 @@
 package entity
 
+import "errors"
+
 type Order struct {
 	ID         string
 	Price      float64
 	Tax        float64
 	finalPrice float64
+}
+
+func NewOrder(id string, price float64, tax float64) (*Order, error) {
+	order := &Order{
+		ID:    id,
+		Price: price,
+		Tax:   tax,
+	}
+	err := order.IsValid()
+	if err != nil {
+		return nil, err
+	}
+	return order, nil
+}
+
+func CalculateFinalPrice(order *Order) {
+	order.finalPrice = order.Price + order.Tax
+}
+
+func (o *Order) IsValid() error {
+	if o.ID == "" {
+		return errors.New("invalid id")
+	}
+
+	if o.Price == 0 {
+		return errors.New("invalid price")
+	}
+
+	if o.Tax == 0 {
+		return errors.New("invalid tax")
+	}
+	return nil
 }
--- order/entity/order_test.go
+++ order/entity/order_test.go
@@ -1,1 +1,46 @@
 package entity_test
+
+import (
+	"testing"
+
+	"github.com/hgribeiro/aluno-go/order/entity"
+	"github.com/stretchr/testify/assert"
+)
+
+// // NOME DO PACOTE + NOME DA STRUCK + NOME DO QUE VAI FAZER
+// É UMA POSSIBILIDADE, MAS VOU POSSO USAR O
+// given_when_then
+// ENTREGUE XXXX PARAMENTRO -- QUANDO QUAL ACONTECER -- ENTÃO O QUE DEVE ACONTECER
+func TestGivingAnEmpyId_WhenCreateANewOrder_ThemShouldReceiveAnError(t *testing.T) {
+
+	order := entity.Order{}
+	assert.Error(t, order.IsValid(), "invalid id")
+}
+
+func TestGivingAnEmpyPrice_WhenCreateANewOrder_ThemShouldReceiveAnError(t *testing.T) {
+
+	order := entity.Order{ID: "123"}
+	assert.Error(t, order.IsValid(), "invalid price")
+}
+
+func TestGivingAnEmpyTax_WhenCreateANewOrder_ThemShouldReceiveAnError(t *testing.T) {
+
+	order := entity.Order{ID: "123", Price: 10}
+	assert.Error(t, order.IsValid(), "invalid tax")
+}
+
+func TestGivenValidParams_WhenCreateANewOrder_ThemShouldNotReceiveCreateORderWithAllParams(t *testing.T) {
+	order, err := entity.NewOrder("123", 10, 1)
+	assert.NoError(t, err)
+	assert.Equal(t, "123", order.ID)
+	assert.Equal(t, 10.0, order.Price)
+	assert.Equal(t, 1.0, order.Tax)
+}
+
+func TestGivenAValidParams_WhenCallculateFinalPrice_ThenShouldCalculateFinalPriceAndSetItOnFinalPricePropety(t *testing.T) {
+	order, err := entity.NewOrder("123", 10, 1)
+	assert.NoError(t, err)
+	err = order.CalculateFinalPrice()
+	assert.NoError(t, err)
+	assert.Equal(t, 11.0, order.FinalPrice)
+}
